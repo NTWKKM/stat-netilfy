0c4d822dd1a919d42e478aff6cff0dec778f1d04
Nattawit <201893855+NTWKKM@users.noreply.github.com>
2025-12-16 13:37:50 +0700

    Update logger.py

--- logger.py
+++ logger.py
@@ -26,7 +26,7 @@ import logging.handlers
 import sys
 from pathlib import Path
 from contextlib import contextmanager
-from typing import Optional, Dict, Any
+from typing import Optional, Any
 import time
 import traceback
 from datetime import datetime
