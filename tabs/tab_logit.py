d0c7131893537cc1d85ca405bbc73123810ed357
Nattawit <201893855+NTWKKM@users.noreply.github.com>
2025-12-24 09:16:18 +0700

    Update tab_logit.py

--- tabs/tab_logit.py
+++ tabs/tab_logit.py
@@ -70,7 +70,7 @@ def _render_logit_subgroup_analysis(df: pd.DataFrame):
     with st.container():
         col1, col2 = st.columns([1, 4])
         with col1:
-            st.info("🚀", help="Click for more information")
+            st.info("🚀")
         with col2:
             st.markdown("""
             **Test for Treatment-by-Subgroup Interaction**
